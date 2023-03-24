2358bf463209b96cb036c83cf599bdbc9f566990
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-03-24 17:30:10 +0000

    started extra

---
+++ go_app/main.go
@@ -0,0 +1,31 @@
+// Created by: Dominic M.
+// Created on: March 2023
+//
+// This program finds the area and perimeter of a rectangle
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function finds the area and perimeter of a rectangle
+	var hour int
+	var pays int
+	var TAX_RATE int
+
+	// input
+	fmt.Println("This program finds how much money you take home and how much the government gets")
+	fmt.Println()
+	fmt.Print("How many hours do you work: ")
+	fmt.Scanln(&hour)
+	fmt.Print("What is your pay: ")
+	fmt.Scanln(&pays)
+
+	// process
+	area = (base * height) / 2
+
+	// output
+	fmt.Println()
+	fmt.Println("The area is:", area, "cm².")
+	fmt.Println("\nDone.")
+}
