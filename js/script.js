a25871c9f2054cbb746e157ffef61a40a2c5c4c1
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-06-04 00:59:19 +0000

    run workflows

--- js/script.js
+++ js/script.js
@@ -3,27 +3,27 @@
 // Created by: Dominic M.
 // Created on: March 2023
 // This file contains the JS functions for index.html
-'use strict'
+"use strict"
 /**
  * This function calculates area and perimeter of rectangle.
  */
 
 function myInfo() {
   if (localStorage.pay) {
-    document.getElementById('pay').value = localStorage.pay
+    document.getElementById("pay").value = localStorage.pay
   }
   if (localStorage.hours) {
-    document.getElementById('hours').value = localStorage.hours
+    document.getElementById("hours").value = localStorage.hours
   }
   if (localStorage.pay && localStorage.hours) {
     myButtonClicked()
   }
 }
 
-function myButtonClicked () {
+function myButtonClicked() {
   // input
-  const hours = parseFloat(document.getElementById('hours').value)
-  const payday = parseFloat(document.getElementById('pay').value)
+  const hours = parseFloat(document.getElementById("hours").value)
+  const payday = parseFloat(document.getElementById("pay").value)
 
   localStorage.pay = payday
   localStorage.hours = hours
@@ -35,6 +35,8 @@ function myButtonClicked () {
   const you_get = pay_no_tax - tax
 
   // output
-  document.getElementById('your-pay').innerHTML = 'Your pay will be: $' + you_get.toFixed(2)
-  document.getElementById('their-pay').innerHTML = 'The government will take: $' + tax.toFixed(2)
+  document.getElementById("your-pay").innerHTML =
+    "Your pay will be: $" + you_get.toFixed(2)
+  document.getElementById("their-pay").innerHTML =
+    "The government will take: $" + tax.toFixed(2)
 }
